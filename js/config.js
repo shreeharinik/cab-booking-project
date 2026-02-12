6b4b1af1e76f9d3bf7e76dabb384cebbe7e75607
shree <159869108+shreeharinik@users.noreply.github.com>
2026-02-12 16:19:35 +0000

    Update configuration to include Cognito settings

--- js/config.js
+++ js/config.js
@@ -1,7 +1,13 @@
 window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_cNmoeZpNj',
+        userPoolClientId: 'n21jtsp78vi7le6jll2tv91q1',
+        region: 'ap-south-1'
+    },
     api: {
         invokeUrl: 'https://zsievbkdqg.execute-api.ap-south-1.amazonaws.com/sbi' // Replace with your API URL
     }
 };
+
 
 
